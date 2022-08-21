6b007e4352cd057693deb0554d26eec4c28a6313
D065023 <david.kunz@sap.com>
2022-08-21 13:33:24 +0200

    version

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ function _main(cb) {
 function main() {
   const runtimeArgs = process.argv.slice(2)
   if (runtimeArgs[0] === '--version' || runtimeArgs[0] === '-v') {
-    console.log('Derive-Type Version 0.0.5')
+    console.log('Derive-Type Version 0.0.6')
     return
   }
   dbg('runtime arguments:', runtimeArgs)
