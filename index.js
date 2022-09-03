55f7a23e601a07a53717fe73c985a9670bb53f60
D065023 <david.kunz@sap.com>
2022-09-03 09:26:57 +0200

    version bump

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ function _main(cb) {
 }
 
 function main() {
-  const version = '0.0.99'
+  const version = '0.0.991'
   const runtimeArgs = process.argv.slice(2)
   if (runtimeArgs[0] === '--version' || runtimeArgs[0] === '-v') {
     console.log('Derive-Type Version', version)
